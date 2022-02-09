d995e4ba74c19147a7a0f397abde958a04c9d854
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2022-02-09 11:38:01 +0100

    squashed single decouple commit

---
+++ src/Spryker/Glue/GlueJsonApiConventionExtension/Dependency/Plugin/RequestAfterRoutingValidatorPluginInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\GlueJsonApiConventionExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\GlueRequestTransfer;
+use Generated\Shared\Transfer\GlueRequestValidationTransfer;
+
+/**
+ * Use this plugin for JSON API convention to validate headers after routing.
+ */
+interface RequestAfterRoutingValidatorPluginInterface
+{
+    /**
+     * Specification:
+     * - Execute validations that need to be aware of the resolved route.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\GlueRequestTransfer $glueRequestTransfer
+     * @param \Spryker\Glue\GlueJsonApiConventionExtension\Dependency\Plugin\JsonApiResourceInterface $jsonApiResource
+     *
+     * @return \Generated\Shared\Transfer\GlueRequestValidationTransfer
+     */
+    public function validateRequest(GlueRequestTransfer $glueRequestTransfer, JsonApiResourceInterface $jsonApiResource): GlueRequestValidationTransfer;
+}
